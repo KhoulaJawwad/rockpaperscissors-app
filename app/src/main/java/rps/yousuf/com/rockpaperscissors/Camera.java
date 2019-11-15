6a651a44d025831b61ce19d9a0430b52e3c5d162
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-11-15 10:40:47 -0500

    Add files via upload

---
+++ app/src/main/java/rps/yousuf/com/rockpaperscissors/Camera.java
@@ -0,0 +1,30 @@
+package rps.yousuf.com.rockpaperscissors;
+
+public class Camera
+{
+    // Instance Variables
+
+    // Getters and Setters
+
+
+    // Other Methods
+
+    /**
+     * Gets the Live feed from the camera
+     * */
+    public void getLiveFeed()
+    {
+
+    }
+
+    /**
+     * Snaps a picture from the camera and sends it
+     * */
+    public void getPic()
+    {
+
+    }
+
+
+
+}
